88c22720186419935b2adf1a5d5d885b0ef37eb6
Gaurav-py-Ghosh <gauravghosh24005@gmail.com>
2025-04-10 20:24:37 +0530

    implemented map and placing one emoji on the map

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,169 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Emoji Exp',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MapScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+  
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MapScreen> createState() => _MapScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class _MapScreenState extends State<MapScreen> with TickerProviderStateMixin {
+  late GoogleMapController _mapController;
+  final Set<Marker> _markers = {};
+  late Marker _animatedMarker;
+  BitmapDescriptor? _emojiIcon;
+  
+  // Default location (San Francisco) before real location is fetched.
+  final LatLng _currentLocation = const LatLng(37.7749, -122.4194);
+  
+  AnimationController? _animationController;
+  Animation<double>? _latAnimation;
+  Animation<double>? _lngAnimation;
+  
+  @override
+  void initState() {
+    super.initState();
+    _loadEmojiIcon();
+    _fetchCurrentLocation();
+  }
+  
+  /// Loads the emoji icon from assets.
+  Future<void> _loadEmojiIcon() async {
+    BitmapDescriptor.fromAssetImage(
+      const ImageConfiguration(size: Size(12, 12)),
+      'assets/emoji.png',
+    ).then((icon) {
+      setState(() {
+        _emojiIcon = icon;
+      });
     });
   }
 
+  
+  /// Fetches the current location using the location package.
+  Future<void> _fetchCurrentLocation() async {
+    Location location = Location();
+    
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) return;
+    }
+    
+    PermissionStatus permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) return;
+    }
+    
+    LocationData locData = await location.getLocation();
+    LatLng newLocation = LatLng(locData.latitude!, locData.longitude!);
+    _animateMarker(newLocation);
+    _mapController.animateCamera(CameraUpdate.newLatLngZoom(newLocation, 14.0));
+  }
+  
+  /// Smoothly animates the emoji marker from its current position to [destination].
+  void _animateMarker(LatLng destination) {
+    // Get the starting position of the current animated marker.
+    final startLat = _animatedMarker.position.latitude;
+    final startLng = _animatedMarker.position.longitude;
+    
+    // Dispose of any previous animation.
+    _animationController?.dispose();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    
+    // Create tweens for latitude and longitude.
+    _latAnimation = Tween<double>(begin: startLat, end: destination.latitude)
+      .animate(_animationController!);
+    _lngAnimation = Tween<double>(begin: startLng, end: destination.longitude)
+      .animate(_animationController!);
+    
+    _animationController!.addListener(() {
+      LatLng newPos = LatLng(_latAnimation!.value, _lngAnimation!.value);
+      setState(() {
+        // Remove the old marker and add a new one at the updated position.
+        _markers.removeWhere((marker) => marker.markerId == const MarkerId('animatedMarker'));
+        _animatedMarker = Marker(
+          markerId: const MarkerId('animatedMarker'),
+          position: newPos,
+          icon: _emojiIcon ?? BitmapDescriptor.defaultMarker,
+          infoWindow: const InfoWindow(title: 'Emoji Marker'),
+        );
+        _markers.add(_animatedMarker);
+      });
+    });
+    
+    _animationController!.forward();
+  }
+  
+  @override
+  void dispose() {
+    _animationController?.dispose();
+    _mapController.dispose();
+    super.dispose();
+  }
+  
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+    // Initialize the animated marker.
+    _animatedMarker = Marker(
+      markerId: const MarkerId('animatedMarker'),
+      position: _currentLocation,
+      icon: _emojiIcon ?? BitmapDescriptor.defaultMarker,
+      infoWindow: const InfoWindow(title: 'Emoji Marker'),
+    );
+    setState(() {
+      _markers.add(_animatedMarker);
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Emoji Exp Map"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _currentLocation,
+          zoom: 14.0,
         ),
+        markers: _markers,
+        onMapCreated: _onMapCreated,
+        // Tapping on the map moves the emoji marker smoothly to that location.
+        onTap: (LatLng tappedPoint) {
+          _animateMarker(tappedPoint);
+        },
+        myLocationEnabled: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _fetchCurrentLocation,
+        child: const Icon(Icons.my_location),
+      ),
     );
   }
 }
